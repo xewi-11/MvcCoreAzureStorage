5415cd9727f780c1711e13bf173e623a81bc7c82
xewi-11 <javi11coimbra@gmail.com>
2026-04-14 12:32:35 +0200

    Add ApiTokensAzureStorage Web API with SAS token service
    Added new ASP.NET Core Web API project ApiTokensAzureStorage to the solution. Includes configuration for Azure Table Storage, a minimal /weatherforecast endpoint, and a ServiceSaSToken class for generating read-only SAS tokens scoped by partition key. Added HTTP test file and updated solution file to reference the new project.

---
+++ ApiTokensAzureStorage/Services/ServiceSaSToken.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Data.Tables;
+using Azure.Data.Tables.Sas;
+
+namespace ApiTokensAzureStorage.Services
+{
+    public class ServiceSaSToken
+
+
+    {
+        private TableClient tableClient;
+
+        public ServiceSaSToken(IConfiguration configuration)
+        {
+            string azureKey = configuration.GetValue<string>("AzureKeys:StorageAccount");
+            TableServiceClient serviceClient = new TableServiceClient(azureKey);
+            this.tableClient = serviceClient.GetTableClient("alumnos");
+        }
+
+        public string GenerateToken(string curso)
+        {
+            //NECESITAMOS LOS PERMISOS DE ACCESO PARA EL TOKEN, EN ESTE CASO SOLO LECTURA
+
+            TableSasPermissions permisos = TableSasPermissions.Read;
+
+            //EL ACCESO A TOKEN ESTA DELIMITADO POR UN TIEMPO DETERMINADO
+
+            TableSasBuilder builder = this.tableClient.GetSasBuilder(permisos, DateTimeOffset.UtcNow.AddMinutes(30));
+
+            //EL ACCESO A LOS DATOS ES MEDIANTE ROW KEY O PARTITION KEY, SON STRING Y VAN DE FORMA ALFABETICA
+
+            builder.PartitionKeyStart = curso;
+            builder.PartitionKeyEnd = curso;
+            //YA TENEMOS EL TOKEN QUE ES UN ACCESO MEDIANTE URI
+
+            Uri uriToken = this.tableClient.GenerateSasUri(builder);
+            string token = uriToken.AbsoluteUri;
+            return token;
+        }
+    }
+}
